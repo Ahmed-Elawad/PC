05e2903195e407148d77a47741855c37c7d078bd
A E <a.elawad97@gmail.com>
2023-06-14 20:43:59 -0400

    int conversion to binary

--- PCStack/Lib/Assembler/Assembler.cpp
+++ PCStack/Lib/Assembler/Assembler.cpp
@@ -20,10 +20,7 @@ int main() {
     size_t n = 16;
     int precision = n - std::min(n, symbol.size());
     std::string a_instruction = std::string(precision, '0').append(symbol);
-    std::cout << "a_instruction : " << a_instruction  << std::endl; // 
     // a_instruction : 0000000000000016 need to convert to binary
-
-
 
     parser.advance(); // advance to the c command
     std::string comp =  parser.comp();
@@ -31,12 +28,7 @@ int main() {
     std::string jmp =  parser.jump();
 
     // combine the instruction
+    // don't always want to construct if there is no c instruction
     std::string c_instruction = "111" + codeTables.aCode(comp) + codeTables.dest(dest) + codeTables.comp(comp) + codeTables.jump(jmp);
-    std::cout << "c_instruction : " << c_instruction  << std::endl; // dest=comp;jump
-    // terminate called after throwing an instance of 'std::logic_error'
-    // what():  basic_string::_M_construct null not valid
-    // caused by line M=1, has no jump clause(;jmp)
-    // need to correct parser to handle null inputs while searching for a string
-
     return 0;
 }
--- PCStack/Lib/Assembler/Parser.cpp
+++ PCStack/Lib/Assembler/Parser.cpp
@@ -4,7 +4,6 @@ class Parser
 public:
     Parser(std::string &fileName)
     {
-        std::cout << "Parser constructor " << fileName << std::endl;
         readFromFile.open(fileName, readFromFile.in);
     };
 
@@ -15,18 +14,13 @@ public:
         L_COMMAND
     };
 
-    bool hasMoreCommands()
-    {
-        return !readFromFile.eof();
-    };
 
     void advance()
     {
-        if (!hasMoreCommands())
+        if (readFromFile.eof())
             return;
 
         std::getline(readFromFile, current_command, '\n');
-        std::cout << current_command << std::endl;
         setCommantType();
     };
 
@@ -52,16 +46,36 @@ public:
         if (current_command_type == CommandType::C_COMMAND)
             return "NULL";
 
-        if (current_command_type == CommandType::A_COMMAND)
-        {
-            return current_command.substr(1, current_command.length() - 1);
-        }
 
         if (current_command_type == CommandType::L_COMMAND)
             return "NULL";
 
+        if (current_command_type == CommandType::A_COMMAND)
+        {
+            // if the table contains the sybmol return it's pointer
+            // if the symbol is a number return its binary form
+            // convert the string to an int
+            // convert the int to it's binary form string
+            // couple of ways to do this. Read the string from right to left and do bitshifts, use a library
+            std::string symbol = current_command.substr(1, current_command.length() - 1);
+            int intSymbol = std::stoi(symbol); // what happens if it isn't an int??
+            // do the loop to build the result
+            std::string bit_form = "";
+            while (intSymbol != 0) {
+                int remainder = intSymbol % 2;
+                if (remainder != 0) {
+                    bit_form = "1" + bit_form;
+                    intSymbol--;
+                }
+                else bit_form = "0" + bit_form;
+                intSymbol /= 2;
+            }
+            return bit_form;
+        }
+
         return "NULL";
     };
+
 
     std::string dest()
     {
@@ -75,17 +89,23 @@ public:
     {
         if (current_command_type != CommandType::C_COMMAND)
             return "NULL";
-            // mmight not always have a jump
-        return current_command.substr(current_command.find('=') + 1, current_command.find(';') - 2);
+
+        int end = current_command.find(';');
+        int start = current_command.find('=');
+        if (end == std::string::npos) return "NULL";
+
+        return current_command.substr(start + 1, end - 2);
     };
 
     std::string jump()
     {
         if (current_command_type != CommandType::C_COMMAND)
             return "NULL";
-        // define some constant to indicate an non found value
-        if (current_command.find(';') > current_command.length()) return nullptr;
-        return current_command.substr(current_command.find(';') + 1, current_command.length() - 1);
+
+        int start = current_command.find(';');
+        if (start == std::string::npos) return "NULL";
+
+        return current_command.substr(start + 1, current_command.length() - 1);
     };
 
     // cleanup
