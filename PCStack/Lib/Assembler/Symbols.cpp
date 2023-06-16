419357183d395b88ef035c06fa2e894419471a7c
A E <a.elawad97@gmail.com>
2023-06-15 21:08:48 -0400

    implement symbols logic

--- PCStack/Lib/Assembler/Symbols.cpp
+++ PCStack/Lib/Assembler/Symbols.cpp
@@ -4,18 +4,40 @@
 class Symbols
 {
 public:
-    Symbols();
+    Symbols(){
+        symbolsTable.insert({"SP", 0});
+        symbolsTable.insert({"LCL", 1});
+        symbolsTable.insert({"ARG", 2});
+        symbolsTable.insert({"THIS", 3});
+        symbolsTable.insert({"THAT", 4});
+        // this is a range so it needs to be defined as such
+        symbolsTable.insert({"RO-R15", 0-15});
+        symbolsTable.insert({"SCREEN", 16384});
+        symbolsTable.insert({"KBD", 24576});
+    };
 
-    void addEntry();
-    void contains();
-    void getAddress();
+    void addEntry(std::string &symbol, int address)
+    {
+        if (!contains(symbol)) symbolsTable[symbol] = address;
+    };
 
-    ~Symbols(){
+    bool contains(std::string &symbol)
+    {
+        return symbolsTable.find(symbol) != symbolsTable.end();
+    };
+
+    int getAddress(std::string &symbol) {
+        if (contains(symbol)) return symbolsTable[symbol];
+        return -1;
+    };
+
+    ~Symbols()
+    {
         symbolsTable.clear();
     }
 
 private:
     // convert to hash table at some point
-    int next_address; // next address is the first address available in memory
-    std::map<std::string, std::string> symbolsTable;
+    int next_address = 16; // next address is the first address available in memory
+    std::map<std::string, int> symbolsTable;
 };
