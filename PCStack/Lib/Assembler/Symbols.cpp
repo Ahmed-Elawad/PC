192486ff9d4085a107febe6d6c7652486be5a65f
A E <a.elawad97@gmail.com>
2023-06-18 19:16:02 -0400

    refactor symbols logic

--- PCStack/Lib/Assembler/Symbols.cpp
+++ PCStack/Lib/Assembler/Symbols.cpp
@@ -11,14 +11,32 @@ public:
         symbolsTable.insert({"THIS", 3});
         symbolsTable.insert({"THAT", 4});
         // this is a range so it needs to be defined as such
-        symbolsTable.insert({"RO-R15", 0-15});
+        symbolsTable.insert({"R0", 0});
+        symbolsTable.insert({"R1", 1});
+        symbolsTable.insert({"R2", 2});
+        symbolsTable.insert({"R3", 3});
+        symbolsTable.insert({"R4", 4});
+        symbolsTable.insert({"R5", 5});
+        symbolsTable.insert({"R6", 6});
+        symbolsTable.insert({"R7", 7});
+        symbolsTable.insert({"R8", 8});
+        symbolsTable.insert({"R9", 9});
+        symbolsTable.insert({"R10", 10});
+        symbolsTable.insert({"R11", 11});
+        symbolsTable.insert({"R12", 12});
+        symbolsTable.insert({"R13", 13});
+        symbolsTable.insert({"R13", 13});
+        symbolsTable.insert({"R14", 14});
+        symbolsTable.insert({"R15", 15});
+        
         symbolsTable.insert({"SCREEN", 16384});
         symbolsTable.insert({"KBD", 24576});
     };
 
     void addEntry(std::string &symbol, int address)
     {
-        if (!contains(symbol)) symbolsTable[symbol] = address;
+        std::cout << "adding symbol - " << symbol << " - " << address << std::endl;
+        symbolsTable[symbol] = address;
     };
 
     bool contains(std::string &symbol)
@@ -31,6 +49,12 @@ public:
         return -1;
     };
 
+    int getNextAddress() {
+        int ret_val = next_address;
+        next_address++;
+        return ret_val;
+    }
+    
     ~Symbols()
     {
         symbolsTable.clear();
